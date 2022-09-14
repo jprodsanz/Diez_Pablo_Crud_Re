89db5190b3f23c563668e85e5caf1b1259962af0
Pablo X <jprodsanz@gmail.com>
2022-09-14 10:39:33 -0500

    crud mod

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -8,7 +8,10 @@ class User:
         self.email  = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']  
-    
+
+    def __repr__(self) -> str:
+        return f"User('{self.first_name}', '{self.last_name}' '{self.email}')"
+
     @classmethod 
     def get_all(cls):
         query = "SELECT * FROM users;"
@@ -24,3 +27,20 @@ class User:
 
         result = connectToMySQL('users_schema').query_db(query,data)
         return result
+
+    @classmethod
+    def get_one(cls,data):
+        query = "SELECT * FROM users WHERE id = %(id)s"
+        result = connectToMySQL('users_schema').query_db(query, data)
+        return cls(result[0])
+    
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s, updated_at=NOW() Where id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
+    
+    @classmethod
+    def destroy(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
+        
